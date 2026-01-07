b9f27c5dd8e03fdabcf35e4d628957fbdbd97094
Sagar Khatri <khatrisagar2@gmail.com>
2026-01-07 11:55:21 +0530

    Login Form UI Changes

--- app/src/main/java/gpg/finalapp/MainActivity.java
+++ app/src/main/java/gpg/finalapp/MainActivity.java
@@ -1,6 +1,10 @@
 package gpg.finalapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        login = findViewById(R.id.main_login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*System.out.println("Login Successfully");
+                Log.d("LOGIN","Login Successfully");
+                Log.e("LOGIN","Login Successfully");*/
+
+                Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_LONG).show();
+                Snackbar.make(view,"Login Successfully",Snackbar.LENGTH_SHORT).show();
+                
+            }
+        });
+
     }
+
+
+
 }
