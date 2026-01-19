13ed004331d1d4a5735b2d6ee8cb33538fbb3439
Sagar Khatri <khatrisagar2@gmail.com>
2026-01-19 12:19:02 +0530

    Signup Changed

--- app/src/main/java/gpg/finalapp/SignupActivity.java
+++ app/src/main/java/gpg/finalapp/SignupActivity.java
@@ -1,14 +1,45 @@
 package gpg.finalapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+
 public class SignupActivity extends AppCompatActivity {
+
+    Button signup;
+    EditText name,email,contact,password,confirmPassword;
+    TextView login;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    RadioGroup gender;
+    CheckBox terms;
+    String sGender;
+
+    Spinner spinner;
+    //String[] cityArray = {"Select City","Ahmedabad","Vadodara","Surat","Rajkot"};
+    ArrayList<String> cityArray;
+    String sCity = ""; //Vadodara
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +51,128 @@ public class SignupActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        name = findViewById(R.id.signup_name);
+        email = findViewById(R.id.signup_email);
+        contact = findViewById(R.id.signup_contact);
+        password = findViewById(R.id.signup_password);
+        confirmPassword = findViewById(R.id.signup_confirm_password);
+        signup = findViewById(R.id.signup_sign_up);
+        login = findViewById(R.id.signup_signin);
+        terms = findViewById(R.id.signup_terms);
+
+        spinner = findViewById(R.id.signup_city);
+
+        cityArray = new ArrayList<>();
+        cityArray.add("Ahmedabad");
+        cityArray.add("Vadodara");
+        cityArray.add("Demo");
+        cityArray.add("Gandhinagar");
+        cityArray.add("Rajkt");
+
+        cityArray.remove(2);
+        cityArray.set(3,"Rajkot");
+
+        cityArray.add(0,"Select City");
+
+        ArrayAdapter adapter = new ArrayAdapter(SignupActivity.this, android.R.layout.simple_list_item_1,cityArray);
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_checked);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i==0){
+                    sCity = "";
+                }
+                else {
+                    //sCity = cityArray[i];
+                    sCity = cityArray.get(i);
+                    Toast.makeText(SignupActivity.this, sCity, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        gender = findViewById(R.id.signup_gender);
+
+        gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull RadioGroup radioGroup, int i) {
+                RadioButton radioButton = findViewById(i);
+                sGender = radioButton.getText().toString();
+                Toast.makeText(SignupActivity.this, sGender, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(name.getText().toString().trim().equals("")){
+                    name.setError("Name Required");
+                }
+                else if(email.getText().toString().trim().equals("")){
+                    email.setError("Email Id Required");
+                }
+                else if(!email.getText().toString().trim().matches(emailPattern)){
+                    email.setError("Valid Email Id Required");
+                }
+                else if(contact.getText().toString().trim().equals("")){
+                    contact.setError("Contact No. Required");
+                }
+                else if(contact.getText().toString().trim().length()<10){
+                    contact.setError("Valid Contact No. Required");
+                }
+                else if(password.getText().toString().trim().equals("")){
+                    password.setError("Password Required");
+                }
+                else if(password.getText().toString().trim().length()<6){
+                    password.setError("Min. 6 Char Password Required");
+                }
+                else if(confirmPassword.getText().toString().trim().equals("")){
+                    confirmPassword.setError("Confirm Password Required");
+                }
+                else if(confirmPassword.getText().toString().trim().length()<6){
+                    confirmPassword.setError("Min. 6 Char Confirm Password Required");
+                }
+                else if(!password.getText().toString().trim().matches(confirmPassword.getText().toString().trim())){
+                    confirmPassword.setError("Password Does Not Match");
+                }
+                else if(gender.getCheckedRadioButtonId() == -1){
+                    Toast.makeText(SignupActivity.this, "Please Select Gender", Toast.LENGTH_SHORT).show();
+                }
+                else if(sCity == ""){
+                    Toast.makeText(SignupActivity.this, "Please Select City", Toast.LENGTH_SHORT).show();
+                }
+                else if(!terms.isChecked()){
+                    Toast.makeText(SignupActivity.this, "Please Accpet Terms & Conditions", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    System.out.println("Signup Successfully");
+                    Log.d("LOGIN", "Signup Successfully");
+                    Log.e("LOGIN", "Signup Successfully");
+                    Log.w("LOGIN", "Signup Successfully");
+
+                    Toast.makeText(SignupActivity.this, "Signup Successfully", Toast.LENGTH_SHORT).show();
+                    Snackbar.make(view, "Signup Successfully", Snackbar.LENGTH_LONG).show();
+
+                    onBackPressed();
+
+                }
+            }
+        });
+
     }
 }
